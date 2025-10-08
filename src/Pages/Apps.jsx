c77ea3f20fc254218c9a38e4d15fee8985d378d0
tahmidman20 <tahmid7282@gmail.com>
2025-10-08 07:17:45 -0600

    Apps search functionality complated

--- src/Pages/Apps.jsx
+++ src/Pages/Apps.jsx
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import useApps from "../hooks/useApps";
 import AppsCard from "../Components/AppsCard";
 
 const Apps = () => {
   const { appsdata } = useApps();
-  console.log(appsdata);
+  const [search, setSearch] = useState("");
+  const term = search.trim().toLocaleLowerCase();
+
+  const searchedApps = term
+    ? appsdata.filter((appData) =>
+        appData.title.toLocaleLowerCase().includes(term)
+      )
+    : appsdata;
+  console.log(searchedApps);
+
   return (
     <div>
       <div className="text-center items-center">
-        <h1>Our All Applications</h1>
-        <p>
+        <h1 className="text-3xl font-bold">Our All Applications</h1>
+        <p className="text-gray-500 pt-2">
           Explore All Apps on the Market developed by us. We code for Millions
         </p>
       </div>
-      <div className="flex justify-between">
-        <h1>This id apps page{appsdata.length}</h1>
-        <input type="search" name="" placeholder="Search" id="" />
+      <div className="flex flex-col sm:flex-row items-start sm:items-center  justify-between mb-5 max-w-screen-2xl w-full px-4 md:px-6 lg:px-8 mx-auto">
+        <h1 className="text-xl font-semibold">
+          ({searchedApps.length})Apps Found
+        </h1>
+        <label className="input">
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            type="search"
+            placeholder="🔍 Search"
+          />
+        </label>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 max-w-screen-2xl w-full px-4 md:px-6 lg:px-8 mx-auto">
-        {appsdata.map((app) => (
-          <AppsCard key={app.id} app={app}></AppsCard>
-        ))}
-      </div>
+      {searchedApps.length === 0 ? (
+        <div className="flex justify-center items-center w-full mx-auto">
+          <img src="/public/App-Error.png" alt="" />
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 max-w-screen-2xl w-full px-4 md:px-6 lg:px-8 mx-auto">
+          {searchedApps.map((app) => (
+            <AppsCard key={app.id} app={app} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
